32e68df6fe2d2c45e3b1b0c69393bf129b7be657
chaitanya-m <writecode101@gmail.com>
2015-06-07 17:51:52 +0530

    Determine win condition. This code is too convoluted. Use html custom data attributes to sort easier.

--- js/puzzle.js
+++ js/puzzle.js
@@ -2,7 +2,6 @@
 $(document).ready(handleResize);
 
 function handleResize() {
-  //console.log($('#board').width());
   $('.box').css('width', $('#board').width() / horizontalBoxNum);
   $('.box').css('height', $('#board').height() / verticalBoxNum);
 }
@@ -10,11 +9,17 @@ function handleResize() {
 verticalBoxNum = 4;
 horizontalBoxNum = 6;
 
-var randomImages=[];
+var shuffledImages=[];
+var shuffledImagesMap={};
+var sortedImages=[];
+
 for (var i = 0; i < verticalBoxNum*horizontalBoxNum; i++) {
-  randomImages.push('url(../tiles/tile_'+i+'.jpg)');
+  image = 'url(../tiles/tile_'+i+'.jpg)'
+  sortedImages.push(image);
+  shuffledImages.push(image);
 }
-randomImages.sort(function() { return 0.5 -Math.random() });
+
+shuffledImages.sort(function() { return 0.5 -Math.random() });
 
 $(document.body).append('<div id="board"></div>');
 
@@ -25,32 +30,49 @@ for (var i = 0; i < verticalBoxNum; i++) {
 
   for(var j = 0; j < horizontalBoxNum; j++) {
     $('#board').append('<div id="box'+ boxCounter +'" class="box"></div>');
-    $('#box'+boxCounter).css('background-image', randomImages[boxCounter]);
+    $('#box'+boxCounter).css('background-image', shuffledImages[boxCounter]);
+    //shuffledImagesMap['#box'+boxCounter] = $('#box'+boxCounter).css('background-image');
+    shuffledImagesMap['#box'+boxCounter] = shuffledImages[boxCounter];
     boxCounter++;
   }
 }
-
+console.log(shuffledImagesMap);
 $('.box').click(handleClick);
 
 var swapStack = [];
 
 function handleClick() {
-  console.log($(this).attr('id'));
   swapStack.push('#'+$(this).attr('id'));
   console.log(swapStack);
   if (swapStack.length == 2) {
     bgImage0 = $(swapStack[0]).css('background-image');
     bgImage1 = $(swapStack[1]).css('background-image');
     if (bgImage0 != bgImage1) {
-      console.log(bgImage0);
-      console.log(bgImage1);
       $(swapStack[0]).css('background-image', bgImage1);
       $(swapStack[1]).css('background-image', bgImage0);
-      console.log($(swapStack[0]).css('background-image'));
-      console.log($(swapStack[1]).css('background-image'));
+
+      temp = shuffledImagesMap[swapStack[0]];
+      shuffledImagesMap[swapStack[0]] = shuffledImagesMap[swapStack[1]];
+      shuffledImagesMap[swapStack[1]] = temp;
+
+      checkWin();
     }
     swapStack=[];
   }
 }
 
+function checkWin() {
+  gameImages = [];
+  for (key in shuffledImagesMap) {
+    gameImages.push(shuffledImagesMap[key]);
+  }
+    console.log(gameImages.toString());
+    console.log(sortedImages.toString());
+  if (gameImages.toString()==sortedImages.toString()) {
+    console.log(":::::::::::::::::::::::::::::::::::::::::::");
+  }
+}
+
+
 window.addEventListener("resize", handleResize);
+
